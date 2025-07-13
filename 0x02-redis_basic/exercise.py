6c72d83addf1339e1cd4e34c56569cc741e89b25
beshegercom@gmail.com <123beshegercom@users.noreply.github.com>
2025-07-13 07:35:46 +0300

    commit again

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,16 +1,25 @@
 #!/usr/bin/env python3
-"""
-Main file
-"""
+"""Cache module"""
+
 import redis
+import uuid
+from typing import Union
 
-Cache = __import__('exercise').Cache
 
-cache = Cache()
+class Cache:
+    def __init__(self):
+        """Initialize Redis client and flush the database"""
+        self._redis = redis.Redis()
+        self._redis.flushdb()
 
-data = b"hello"
-key = cache.store(data)
-print(key)
-
-local_redis = redis.Redis()
-print(local_redis.get(key))
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """
+        Store data in Redis with a random key
+        Args:
+            data: The data to store (str, bytes, int, float)
+        Returns:
+            The key as a string
+        """
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
